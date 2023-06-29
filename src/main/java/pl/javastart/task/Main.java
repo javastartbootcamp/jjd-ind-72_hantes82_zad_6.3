2f52371e876e9c00e20f809e973f6c9e8fbe8bfc
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-06-29 13:34:41 +0200

    Zadanie 6.3

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,27 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        MultiArray multiarray;
+        int arrayRows;
+        int arrayColumns;
+        do {
+            System.out.println("Podaj liczbę wierszy dla nowej tablicy: ");
+            arrayRows = scan.nextInt();
+            scan.nextLine();
+        } while (arrayRows > 100);
+        do {
+            System.out.println("Podaj liczbę kolumn dla nowej tablicy: ");
+            arrayColumns = scan.nextInt();
+            scan.nextLine();
+        } while (arrayColumns > 100);
 
+        multiarray = new MultiArray(arrayRows, arrayColumns);
+        multiarray.fulfil();
+        multiarray.print();
     }
 }
